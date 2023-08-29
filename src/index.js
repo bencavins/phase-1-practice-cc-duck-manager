7fa1e261ca12b964276eea034d34958e6628268c
Ben Cavins <ben.cavins@flatironschool.com>
2023-08-29 11:56:58 -0400

    do lab

--- src/index.js
+++ src/index.js
@@ -8,18 +8,7 @@ fetch("http://localhost:3000/ducks")
 .then(resp => resp.json())
 .then(data => {
     data.forEach(duck => {
-        const duckImg = document.createElement('img')
-        duckImg.src = duck.img_url
-        duckImg.alt = duck.name  // not required
-
-        duckImg.addEventListener('click', event => {
-            duckName.textContent = duck.name
-            duckDisplayImg.src = duck.img_url
-            duckDisplayImg.alt = duck.name
-            duckLikes.textContent = duck.likes
-        })
-
-        duckNav.append(duckImg)
+        addDuckNav(duck)
     })
 })
 
@@ -33,16 +22,28 @@ duckForm.addEventListener('submit', event => {
     event.preventDefault()
     const duckInputName = event.target['duck-name-input'].value
     const duckImg = event.target['duck-image-input'].value
-    
-    const newDuckImg = document.createElement('img')
-    newDuckImg.src = duckImg
-    newDuckImg.alt = duckInputName // not required
 
-    newDuckImg.addEventListener('click', event => {
-        duckName.textContent = duckInputName
-        duckDisplayImg.src = duckImg
-        duckDisplayImg.alt = duckInputName
-        duckLikes.textContent = 0
-    })
-    duckNav.append(newDuckImg)
+    const newDuck = {
+        img_url: duckImg,
+        name: duckInputName,
+        likes: 0
+    }
+    
+    addDuckNav(newDuck)
 })
+
+
+function addDuckNav(duck) {
+    const duckImg = document.createElement('img')
+    duckImg.src = duck.img_url
+    duckImg.alt = duck.name  // not required
+
+    duckImg.addEventListener('click', event => {
+        duckName.textContent = duck.name
+        duckDisplayImg.src = duck.img_url
+        duckDisplayImg.alt = duck.name
+        duckLikes.textContent = duck.likes
+    })
+    
+    duckNav.append(duckImg)
+}
